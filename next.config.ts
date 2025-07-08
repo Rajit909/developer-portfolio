ce9a9f6613920d6288fa8c08952f95f7645cf899
Rajit Kumar <krajit909@gmail.com>
2025-07-08 17:12:30 +0000

    when i'm hiting to signin it throws error like TypeError: Failed to fetc

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev'],
+  allowedDevOrigins: ['*.cloudworkstations.dev', '*.googleusercontent.com'],
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
